f7b4e0822549a23e890d36bac9f4c346118b54da
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2020-04-04 16:22:52 +0200

    Added wake on lan script

---
+++ wake.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+etherwake -i enp0s31f6 8:88:e3:e0:98:9d
+etherwake -i enp0s31f6 0:db:55:e1:cd:98
+etherwake -i enp0s31f6 8:ca:3a:d3:2c:3b
+etherwake -i enp0s31f6 0:1f:af:22:3a:55
+etherwake -i enp0s31f6 0:1f:af:2f:2f:9e
+etherwake -i enp0s31f6 4:be:d9:87:b8:23
+
